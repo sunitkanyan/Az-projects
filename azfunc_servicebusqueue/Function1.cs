1c8f30124bde9054bfaf6e51eed190766583d6a7
Sunit <sunitkanyan@yahoo.com>
2023-02-05 04:59:25 +0530

    CLI Command Service bus and service bus Azure Fun

--- azfunc_servicebusqueue/Function1.cs
+++ azfunc_servicebusqueue/Function1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -12,6 +13,10 @@ namespace azfunc_servicebusqueue
         public static void Run([ServiceBusTrigger("myqueue", Connection = "constring")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+
+            myQueueItem
+
+
         }
     }
 }
--- consoleapp_sendmessage_to SB_queue/Program.cs
+++ consoleapp_sendmessage_to SB_queue/Program.cs
@@ -4,7 +4,11 @@ using Azure.Messaging.ServiceBus;
 Console.WriteLine("Hello, World!");
 
 ServiceBusClient _client = new ServiceBusClient("Endpoint=sb://skkbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=yxv+t8GEqtdtyMArIL4vzqkCOazCLagrk4vN6S9QRHQ=");
-var _sender =_client.CreateSender("myqueue");
+var _sender =_client.CreateSender("myq");
+
+ServiceBusMessage _message = new ServiceBusMessage("this is my text message for this queue");
+_message.ApplicationProperties.Add("Dept", "IT");
+_message.MessageId = "1213";
 
 _sender.SendMessageAsync(new ServiceBusMessage("this is my text message for this queue"));
 Console.ReadLine();
